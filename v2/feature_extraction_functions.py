0c0fd0295730702dbbd11b8830384d169037bebf
Cucklecumber <25mx.free@gmial.com>
2023-02-07 00:29:21 -0500

    d

--- v2/feature_extraction_functions.py
+++ v2/feature_extraction_functions.py
@@ -3,18 +3,4 @@ import numpy as np
 
 
 
-def mfcc_feature_extractor(path):
-    data, simple_rate = lb.load(path, res_type='kaiser_fast')
-    data = lb.feature.mfcc(data, n_mfcc=128)
-    data = np.mean(data,axis=1)
-    return data
-
-
-def stft_feature_extractor(path):
-    data, sample_rate = lb.load(path, res_type='kaiser_fast')
-    stft = np.abs(lb.stft(data, n_fft=128))
-    stft_features = np.mean(stft, axis=1)
-    return stft_features
-
-
 
